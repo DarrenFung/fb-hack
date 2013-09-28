cf7b08a2d7629444e12f63b5289895ca1edec71b
Michael Shi <da.michael.shi@gmail.com>
2013-09-28 14:37:52 -0700

    yay it work

--- extension/app/scripts/contentscript.js
+++ extension/app/scripts/contentscript.js
@@ -2,53 +2,10 @@
 
 console.log("FB Hack Started Loading");
 
-$('#pagelet_home_stream').draggable({
-	  opacity: 0.5,
-	  cursor: 'crosshair',
-      revert: "invalid"
-    });
+$("head").append('<style>#rightCol { list-style-type: none; margin: 0; padding: 0; width: 450px; }</style>');
 
-$('#pagelet_reminders').draggable({
-	opacity: 0.5,
-	cursor: 'crosshair',
-	revert: "invalid"
-});
-
-$('#pagelet_bluebar').draggable({
-	opacity: 0.5,
-	cursor: 'crosshair',
-	revert: "invalid"
-});
-
-$('#pagelet_friends_online').draggable({
-	opacity: 0.5,
-	cursor: 'crosshair',
-	revert: "invalid"
-});
-
-$('#pagelet_bookmark_nav').draggable({
-	opacity: 0.5,
-	cursor: 'crosshair',
-	revert: "invalid"
-});
-
-$('#pagelet_composer').draggable({
-	opacity: 0.5,
-	cursor: 'crosshair',
-	revert: "invalid"
-});
-
-$('#pagelet_sidebar').draggable({
-	opacity: 0.5,
-	cursor: 'crosshair',
-	revert: "invalid"
-});
-
-$('#pagelet_dock').draggable({
-	opacity: 0.5,
-	cursor: 'crosshair',
-	revert: "invalid"
-});
+$("#rightCol div div").sortable();
+$("#rightCol div div").disableSelection();
 
 console.log("FB Hack Finished Loading");
 /*
