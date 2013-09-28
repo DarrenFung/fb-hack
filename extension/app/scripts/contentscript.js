e9586bd18395bd101ac98441015fcfd1caed8159
Michael Shi <da.michael.shi@gmail.com>
2013-09-28 12:54:10 -0700

    omgz

--- extension/app/scripts/contentscript.js
+++ extension/app/scripts/contentscript.js
@@ -1,3 +1,111 @@
 'use strict';
 
-console.log("FB Hack loaded");
+console.log("FB Hack Started Loading");
+
+/*
+// pagelet_bluebar
+$("#pagelet_bluebar").addClass("widget_base");
+$("#pagelet_bluebar").attr({'data-row' : '1', 'data-col' : '1', 'data-sizex':'32', 'data-sizey':'1'});
+// pagelet_bluebar
+
+
+// globalContainer
+$("#globalContainer").addClass("widget_base");
+$("#globalContainer").attr({'data-row' : '2', 'data-col' : '1', 'data-sizex':'32', 'data-sizey':'18'});
+// globalContainer
+
+// composer
+$("#pagelet_composer").addClass("widget_content");
+$("#pagelet_composer").attr({'data-row' : '1', 'data-col' : '1', 'data-sizex':'9', 'data-sizey':'1'});
+// composer
+
+// home_stream
+$("#pagelet_home_stream").addClass("widget_content");
+$("#pagelet_home_stream").attr({'data-row' : '2', 'data-col' : '1', 'data-sizex':'9', 'data-sizey':'12'});
+// home_stream
+
+
+$(".widget_base").gridster({
+    widget_margins: [0, 5],
+    widget_base_dimensions: [40, 40],
+    widget_selector: "div",
+    min_cols: 1
+}).data("gridster");
+
+$(".widget_content").gridster({
+    widget_margins: [0, 5],
+    widget_base_dimensions: [60, 60],
+    widget_selector: "div",
+    min_cols: 1
+}).data("gridster");
+*/
+
+/*
+// leftCol
+$("#leftCol").addClass("widget_content");
+$("#leftCol").attr({'data-row' : '1', 'data-col' : '1', 'data-sizex':'1', 'data-sizey':'1'});
+// leftCol
+
+// contentArea
+$("#contentArea").addClass("widget_content");
+$("#contentArea").attr({'data-row' : '1', 'data-col' : '2', 'data-sizex':'3', 'data-sizey':'1'});
+// contentArea
+
+// rightCol
+$("#rightCol").addClass("widget_content");
+$("#rightCol").attr({'data-row' : '1', 'data-col' : '3', 'data-sizex':'1', 'data-sizey':'1'});
+// rightCol
+
+$(".widget_content > div").gridster({
+    widget_margins: [0, 0],
+    widget_base_dimensions: [200, 754],
+    widget_selector: "div",
+    min_cols: 3
+}).data("gridster");
+*/
+
+// pagelet_reminders
+$("#pagelet_reminders").attr({'data-row' : '1', 'data-col' : '1', 'data-sizex':'24', 'data-sizey':'3'});
+// pagelet_reminders
+
+
+// pagelet_ego_pane
+$("#pagelet_ego_pane").attr({'data-row' : '2', 'data-col' : '1', 'data-sizex':'24', 'data-sizey':'56'});
+// pagelet_ego_pane
+
+// pagelet_rhc_footer
+$("#pagelet_rhc_footer").attr({'data-row' : '3', 'data-col' : '1', 'data-sizex':'9', 'data-sizey':'4'});
+// pagelet_rhc_footer
+
+$("#rightCol div").gridster({
+    widget_margins: [0, 0],
+    widget_base_dimensions: [10, 10],
+    widget_selector: "div",
+    min_cols: 1,
+    avoid_overlapped_widgets: true
+}).data("gridster");
+
+
+console.log("FB Hack Finished Loading");
+/*
+pagelet_bluebar
+pagelet_welcome_box
+pagelet_navigation
+pagelet_pinned_nav
+pagelet_bookmark_nav
+pagelet_friends_online
+pagelet_advertiser_panel
+pagelet_rhc_ticker
+pagelet_reminders
+pagelet_ego_pane
+pagelet_rhc_footer
+pagelet_megaphone
+pagelet_composer
+pagelet_home_stream
+pagelet_stream_pager
+pagelet_sidebar
+pagelet_ticker
+pagelet_dock
+ChatTabsPagelet
+BuddylistPagelet
+*/
