7232416909d066ffce8b5c6f0fb358061dc2e329
Michael Shi <da.michael.shi@gmail.com>
2013-09-28 11:00:10 -0700

    include gridster and jquery

--- extension/app/scripts/contentscript.js
+++ extension/app/scripts/contentscript.js
@@ -1,1 +1,7 @@
 'use strict';
+
+$("#pagelet_bluebar").gridster({
+              widget_margins: [0, 0],
+              widget_base_dimensions: [40, 40],
+              min_cols: 6
+          }).data('gridster');
