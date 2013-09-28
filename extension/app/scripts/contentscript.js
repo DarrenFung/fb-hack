3c80f6152569b84f3050d91aba0f90e4fb3fc8ae
Michael Shi <da.michael.shi@gmail.com>
2013-09-28 14:14:23 -0700

    initial jquery ui + some movements

--- extension/app/scripts/contentscript.js
+++ extension/app/scripts/contentscript.js
@@ -2,88 +2,53 @@
 
 console.log("FB Hack Started Loading");
 
+$('#pagelet_home_stream').draggable({
+	  opacity: 0.5,
+	  cursor: 'crosshair',
+      revert: "invalid"
+    });
 
-// pagelet_bluebar
-$("#pagelet_bluebar").attr({'data-row' : '1', 'data-col' : '1', 'data-sizex':'1', 'data-sizey':'1'});
-// pagelet_bluebar
+$('#pagelet_reminders').draggable({
+	opacity: 0.5,
+	cursor: 'crosshair',
+	revert: "invalid"
+});
 
+$('#pagelet_bluebar').draggable({
+	opacity: 0.5,
+	cursor: 'crosshair',
+	revert: "invalid"
+});
 
-// globalContainer
-$("#globalContainer").attr({'data-row' : '2', 'data-col' : '1', 'data-sizex':'1', 'data-sizey':'18'});
-// globalContainer
+$('#pagelet_friends_online').draggable({
+	opacity: 0.5,
+	cursor: 'crosshair',
+	revert: "invalid"
+});
 
-/*
-// composer
-$("#pagelet_composer").addClass("widget_content");
-$("#pagelet_composer").attr({'data-row' : '1', 'data-col' : '1', 'data-sizex':'9', 'data-sizey':'1'});
-// composer
+$('#pagelet_bookmark_nav').draggable({
+	opacity: 0.5,
+	cursor: 'crosshair',
+	revert: "invalid"
+});
 
-// home_stream
-$("#pagelet_home_stream").addClass("widget_content");
-$("#pagelet_home_stream").attr({'data-row' : '2', 'data-col' : '1', 'data-sizex':'9', 'data-sizey':'12'});
-// home_stream
-*/
+$('#pagelet_composer').draggable({
+	opacity: 0.5,
+	cursor: 'crosshair',
+	revert: "invalid"
+});
 
-$("._li").gridster({
-    widget_margins: [5, 5],
-    widget_base_dimensions: [1000, 50],
-    widget_selector: "div"
-}).data("gridster");
+$('#pagelet_sidebar').draggable({
+	opacity: 0.5,
+	cursor: 'crosshair',
+	revert: "invalid"
+});
 
-/*
-$(".widget_content").gridster({
-    widget_margins: [0, 5],
-    widget_base_dimensions: [60, 60],
-    widget_selector: "div",
-    min_cols: 1
-}).data("gridster");
-*/
-
-/*
-// leftCol
-$("#leftCol").addClass("widget_content");
-$("#leftCol").attr({'data-row' : '1', 'data-col' : '1', 'data-sizex':'1', 'data-sizey':'1'});
-// leftCol
-
-// contentArea
-$("#contentArea").addClass("widget_content");
-$("#contentArea").attr({'data-row' : '1', 'data-col' : '2', 'data-sizex':'3', 'data-sizey':'1'});
-// contentArea
-
-// rightCol
-$("#rightCol").addClass("widget_content");
-$("#rightCol").attr({'data-row' : '1', 'data-col' : '3', 'data-sizex':'1', 'data-sizey':'1'});
-// rightCol
-
-$(".widget_content > div").gridster({
-    widget_margins: [0, 0],
-    widget_base_dimensions: [200, 754],
-    widget_selector: "div",
-    min_cols: 3
-}).data("gridster");
-*/
-
-/*
-// pagelet_reminders
-$("#pagelet_reminders").attr({'data-row' : '1', 'data-col' : '1', 'data-sizex':'24', 'data-sizey':'1'});
-// pagelet_reminders
-
-
-// pagelet_ego_pane
-$("#pagelet_ego_pane").attr({'data-row' : '2', 'data-col' : '1', 'data-sizex':'24', 'data-sizey':'15'});
-// pagelet_ego_pane
-
-// pagelet_rhc_footer
-$("#pagelet_rhc_footer").attr({'data-row' : '3', 'data-col' : '1', 'data-sizex':'9', 'data-sizey':'1'});
-// pagelet_rhc_footer
-
-$("#rightCol div").gridster({
-    widget_margins: [0, 0],
-    widget_base_dimensions: [10, 50],
-    widget_selector: "div",
-    avoid_overlapped_widgets: true
-}).data("gridster");
-*/
+$('#pagelet_dock').draggable({
+	opacity: 0.5,
+	cursor: 'crosshair',
+	revert: "invalid"
+});
 
 console.log("FB Hack Finished Loading");
 /*
