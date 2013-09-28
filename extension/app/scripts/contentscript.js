2d31a2750c4c539432cf52f26bdadb9e2d95e736
Rahul Bhattacharya <r3bhatta@gmail.com>
2013-09-28 12:54:57 -0700

    left nav moving..kinda

--- extension/app/scripts/contentscript.js
+++ extension/app/scripts/contentscript.js
@@ -1,3 +1,26 @@
 'use strict';
 
 console.log("FB Hack loaded");
+
+
+/*begin pagelet_navigation*/
+$("#pagelet_pinned_nav").attr({
+	"data-row" : "1",
+	"data-col" : "1",
+	"data-sizex" : "1",
+	"data-sizey" : "1"
+});
+
+$("#pagelet_bookmark_nav").attr({
+	"data-row" : "2",
+	"data-col" : "1",
+	"data-sizex" : "1",
+	"data-sizey" : "3"
+});
+
+$("#pagelet_navigation div").gridster({
+	widget_margins: [0, 0],
+	widget_base_dimensions: [180, 130],
+	widget_selector: "div"
+}).data('gridster');
+/*end pagelet_navigation*/
